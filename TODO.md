dab170c7f83e9e48a44a7ec0544075c674346763
Manish Kumar <manishkr2023.25@gmail.com>
2025-09-07 12:57:49 +0530

    first commit

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO List for Removing Fossify App Features
+
+- [x] Remove "More Fossify Apps" menu item from menu.xml
+- [ ] Remove color customization from SettingsActivity.kt
+- [ ] Remove purchase-related features (Thank You app references)
+- [ ] Update MainActivity.kt to remove related handlers
+- [ ] Test the changes
---
+++ app/src/main/kotlin/org/fossify/phone/extensions/Activity.kt
@@ -0,0 +1,183 @@
+package org.fossify.phone.extensions
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.provider.ContactsContract
+import android.telecom.PhoneAccount
+import android.telecom.PhoneAccountHandle
+import android.telecom.TelecomManager
+import org.fossify.commons.activities.BaseSimpleActivity
+import org.fossify.commons.dialogs.CallConfirmationDialog
+import org.fossify.commons.extensions.initiateCall
+import org.fossify.commons.extensions.isDefaultDialer
+import org.fossify.commons.extensions.isPackageInstalled
+import org.fossify.commons.extensions.launchActivityIntent
+import org.fossify.commons.extensions.launchCallIntent
+import org.fossify.commons.extensions.launchViewContactIntent
+import org.fossify.commons.extensions.telecomManager
+import org.fossify.commons.helpers.CONTACT_ID
+import org.fossify.commons.helpers.IS_PRIVATE
+import org.fossify.commons.helpers.PERMISSION_READ_PHONE_STATE
+import org.fossify.commons.helpers.SimpleContactsHelper
+import org.fossify.commons.helpers.ensureBackgroundThread
+import org.fossify.commons.models.contacts.Contact
+import org.fossify.phone.activities.DialerActivity
+import org.fossify.phone.activities.SimpleActivity
+import org.fossify.phone.dialogs.SelectSIMDialog
+
+fun SimpleActivity.startCallIntent(
+    recipient: String,
+    forceSimSelector: Boolean = false
+) {
+    if (isDefaultDialer()) {
+        getHandleToUse(
+            intent = null,
+            phoneNumber = recipient,
+            forceSimSelector = forceSimSelector
+        ) { handle ->
+            launchCallIntent(recipient, handle)
+        }
+    } else {
+        launchCallIntent(recipient, null)
+    }
+}
+
+fun SimpleActivity.startCallWithConfirmationCheck(
+    recipient: String,
+    name: String,
+    forceSimSelector: Boolean = false
+) {
+    if (config.showCallConfirmation) {
+        CallConfirmationDialog(this, name) {
+            startCallIntent(recipient, forceSimSelector)
+        }
+    } else {
+        startCallIntent(recipient, forceSimSelector)
+    }
+}
+
+fun SimpleActivity.startCallWithConfirmationCheck(contact: Contact) {
+    if (config.showCallConfirmation) {
+        CallConfirmationDialog(
+            activity = this,
+            callee = contact.getNameToDisplay()
+        ) {
+            initiateCall(contact) { launchCallIntent(it) }
+        }
+    } else {
+        initiateCall(contact) { launchCallIntent(it) }
+    }
+}
+
+fun SimpleActivity.launchCreateNewContactIntent() {
+    Intent().apply {
+        action = Intent.ACTION_INSERT
+        data = ContactsContract.Contacts.CONTENT_URI
+        launchActivityIntent(this)
+    }
+}
+
+fun BaseSimpleActivity.callContactWithSim(
+    recipient: String,
+    useMainSIM: Boolean
+) {
+    handlePermission(PERMISSION_READ_PHONE_STATE) {
+        val wantedSimIndex = if (useMainSIM) 0 else 1
+        val handle = getAvailableSIMCardLabels()
+            .sortedBy { it.id }
+            .getOrNull(wantedSimIndex)?.handle
+        launchCallIntent(recipient, handle)
+    }
+}
+
+fun BaseSimpleActivity.callContactWithSimWithConfirmationCheck(
+    recipient: String,
+    name: String,
+    useMainSIM: Boolean
+) {
+    if (config.showCallConfirmation) {
+        CallConfirmationDialog(this, name) {
+            callContactWithSim(recipient, useMainSIM)
+        }
+    } else {
+        callContactWithSim(recipient, useMainSIM)
+    }
+}
+
+// handle private contacts differently, only Simple Contacts Pro can open them
+fun Activity.startContactDetailsIntent(contact: Contact) {
+    val simpleContacts = "org.fossify.contacts"
+    val simpleContactsDebug = "org.fossify.contacts.debug"
+    if (contact.rawId > 1000000 && contact.contactId > 1000000 && contact.rawId == contact.contactId &&
+        (isPackageInstalled(simpleContacts) || isPackageInstalled(simpleContactsDebug))
+    ) {
+        Intent().apply {
+            action = Intent.ACTION_VIEW
+            putExtra(CONTACT_ID, contact.rawId)
+            putExtra(IS_PRIVATE, true)
+            `package` =
+                if (isPackageInstalled(simpleContacts)) simpleContacts else simpleContactsDebug
+            setDataAndType(
+                ContactsContract.Contacts.CONTENT_LOOKUP_URI,
+                "vnd.android.cursor.dir/person"
+            )
+            launchActivityIntent(this)
+        }
+    } else {
+        ensureBackgroundThread {
+            val lookupKey =
+                SimpleContactsHelper(this).getContactLookupKey((contact).rawId.toString())
+            val publicUri =
+                Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookupKey)
+            runOnUiThread {
+                launchViewContactIntent(publicUri)
+            }
+        }
+    }
+}
+
+// used at devices with multiple SIM cards
+@SuppressLint("MissingPermission")
+fun SimpleActivity.getHandleToUse(
+    intent: Intent?,
+    phoneNumber: String,
+    forceSimSelector: Boolean = false,
+    callback: (handle: PhoneAccountHandle?) -> Unit
+) {
+    handlePermission(PERMISSION_READ_PHONE_STATE) {
+        if (it) {
+            val defaultHandle =
+                telecomManager.getDefaultOutgoingPhoneAccount(PhoneAccount.SCHEME_TEL)
+            when {
+                forceSimSelector -> showSelectSimDialog(phoneNumber, callback)
+                intent?.hasExtra(TelecomManager.EXTRA_PHONE_ACCOUNT_HANDLE) == true -> {
+                    callback(intent.getParcelableExtra(TelecomManager.EXTRA_PHONE_ACCOUNT_HANDLE)!!)
+                }
+
+                config.getCustomSIM(phoneNumber) != null -> {
+                    callback(config.getCustomSIM(phoneNumber))
+                }
+
+                defaultHandle != null -> callback(defaultHandle)
+                else -> showSelectSimDialog(phoneNumber, callback)
+            }
+        }
+    }
+}
+
+fun SimpleActivity.showSelectSimDialog(
+    phoneNumber: String,
+    callback: (handle: PhoneAccountHandle?) -> Unit
+) = SelectSIMDialog(
+    activity = this,
+    phoneNumber = phoneNumber,
+    onDismiss = {
+        if (this is DialerActivity) {
+            finish()
+        }
+    }
+) { handle ->
+    callback(handle)
+}
